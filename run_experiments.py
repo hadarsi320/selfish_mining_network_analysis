16447cc8557c04adf95d9d936d3703e9f26bbc81
Hadar <hadarsi320@gmail.com>
2022-07-31 21:32:44 +0300

    Running with pool

--- run_experiments.py
+++ run_experiments.py
@@ -1,3 +1,4 @@
+from multiprocessing import Pool
 from pathlib import Path
 
 import numpy as np
@@ -7,14 +8,14 @@ from utils import convert_args_dict
 
 
 def rr_by_power_exp():
-    default_args = {'num-nodes': 1000, 'turns': 1000, 'num-pools': 5, 'pool-connectivity': 0.5}
-    default_flags = ['dynamic-progress']
-    i = 1
+    default_args = {'num-nodes': 1000, 'turns': 1000, 'num-pools': 5, 'pool-connectivity': 0.5, 'prints': 'parallel'}
+    default_flags = []
+    args_lists = []
     for tie_breaking in ['first', 'random']:
         for selfish_mining in [False]:
             for banning in [False]:
-                for pool_power in np.arange(0.05, 0.51, 0.05):
-                    for seed in range(10):
+                for pool_power in np.arange(0.05, 0.5, 0.05):
+                    for seed in range(5):
                         args = default_args.copy()
                         flags = default_flags.copy()
 
@@ -28,14 +29,15 @@ def rr_by_power_exp():
                         args['outf'] = Path('outputs/{tie-breaking}/{}/{pool-powers}/{seed}/out.json'.format(
                             'selfish' if selfish_mining else 'honest', **args))
 
-                        print('Running experiment number {} with outf {}'.format(i, args['outf']))
+                        # print('Running experiment number {} with outf {}'.format(i, args['outf']))
                         if args['outf'].exists():
                             print('Skipping experiment as outf exists', args['outf'])
                             continue
 
                         cmd_args = convert_args_dict(args, flags)
-                        mining_simulation(cmd_args)
-                        i += 1
+                        args_lists.append(cmd_args)
+    with Pool(5) as p:
+        p.map(mining_simulation, args_lists)
 
 
 if __name__ == '__main__':
